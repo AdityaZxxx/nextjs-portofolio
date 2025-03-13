202cb250e50050925f462f2d6df61af6edefef2c
AdityaZxxx <adityaofficial714@gmail.com>
2025-03-13 19:27:24 +0700

    Fix: Theme preferences 🛠 ️

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body id="root">
+      <body id="root" className="dark">
         <ThemeProvider>
           <MotionFrammerWrapper>
             <ClientLayout>{children}</ClientLayout>
